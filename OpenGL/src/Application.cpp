772dd90e23959d1f282e6f9fac0c2fde86c6b3d6
Cpp基无铅压电陶瓷 <jekyulll@gmail.com>
2024-06-17 14:29:51 +0800

    添加项目文件。

---
+++ OpenGL/src/Application.cpp
@@ -0,0 +1,71 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include<iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    // GLEW
+    if (glewInit() != GLEW_OK)
+        std::cout << "glew init Error!" << std::endl;
+    
+    //  打印版本号
+    std::cout << "GLEW version: " << glGetString(GL_VERSION) << std::endl;
+
+    // 顶点缓冲区
+
+    float position[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT); 
+
+#pragma region 传统OpenGL画一个三角形
+        /*glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();*/
+#pragma endregion
+
+#pragma region 绘制三角形
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+#pragma endregion
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
